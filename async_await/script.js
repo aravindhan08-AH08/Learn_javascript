c91415fe1f38aa623fa33094025c4f6ba4b12890
Aravindhan M V <aravindhanmv2008@gmail.com>
2026-02-23 15:05:42 +0530

    Changes

--- async_await/script.js
+++ async_await/script.js
@@ -1,5 +1,5 @@
 // Fetch code in using Async Await in the fetch code.
-// And then pratice for the Async Await
+// And then practice for the Async Await
 async function loadProducts() {
     try{
         const response = await fetch(

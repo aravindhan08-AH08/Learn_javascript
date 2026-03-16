33c2d3b11416be385b413065922200e2579560f6
Aravindhan M V <aravindhanmv2008@gmail.com>
2026-03-16 14:50:02 +0530

    Add divide and power functions in math.js and toCamelCase utility

---
+++ import-export/app.js
@@ -0,0 +1,13 @@
+import {add, subtract, multiply, divide, power} from "./math.js"
+import {toCamelCase} from "./util.js"
+
+console.log(add(5,3))
+console.log(subtract(10,4))
+console.log(multiply(5,2))
+console.log(divide(10,2))
+console.log(power(2,5))
+// jEeva, Jeeva
+// kaviArasan, Kaviarasan
+console.log(toCamelCase("jEeva"));
+console.log(toCamelCase("kaviyArasan"))
+console.log(toCamelCase("araVindhan"))
---
+++ import-export/util.js
@@ -0,0 +1,3 @@
+export function toCamelCase(s){
+    return s[0].toUpperCase() + s.slice(1).toLowerCase();
+}
